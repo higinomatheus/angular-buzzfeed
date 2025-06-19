5d569d27fc15ca45584f21661b843913550ff923
Matheus Higino <matheus.higino@yahoo.com.br>
2025-06-19 16:52:54 -0300

    criando as propriedades lógicas

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-quiz',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quiz.component.html',
   styleUrl: './quiz.component.css'
 })
 export class QuizComponent {
+	title: string = "";
+	questions: any = [];
+	questionSelected: any = "";
+	answers: string[] = [];
+	answerSelected: string = "";
 
+	questionIndex: number = 0;
+	questionMaxIndex: number = 0;
+
+	finished: boolean = false;
 }

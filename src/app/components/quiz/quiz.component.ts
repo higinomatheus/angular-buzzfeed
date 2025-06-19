6fb172abbc4f7107f55f2f9d21c18563b2e5b0a8
Matheus Higino <matheus.higino@yahoo.com.br>
2025-06-19 17:18:37 -0300

    criando as regras do jogo

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import quiz_question from '../../../assets/data/quiz_questions.json'
 
 @Component({
   selector: 'app-quiz',
@@ -8,7 +9,7 @@ import { Component } from '@angular/core';
   templateUrl: './quiz.component.html',
   styleUrl: './quiz.component.css'
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit {
 	title: string = "";
 	questions: any = [];
 	questionSelected: any = "";
@@ -19,4 +20,28 @@ export class QuizComponent {
 	questionMaxIndex: number = 0;
 
 	finished: boolean = false;
+
+	ngOnInit(): void {
+		if(quiz_question){
+			this.title = quiz_question.title;
+			this.questions = quiz_question.questions;
+			this.questionSelected = this.questions[this.questionIndex];
+
+			this.questionIndex = 0;
+			this.questionMaxIndex = this.questions.length;
+		}
+	}
+
+	selectAnswer(answer: any){
+		this.answers.push(answer);
+	}
+
+	async nextStep(){
+		this.questionIndex++;
+		if(this.questionMaxIndex > this.questionIndex){
+			this.questionSelected = this.questions[this.questionIndex]
+		} else {
+			this.finished = true;
+		}
+	}
 }
